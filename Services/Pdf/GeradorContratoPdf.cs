eba09b813456898d85b858744fc7e6f3e1a29c57
Guilherme-R-Santos <guilhermersantos99@outlook.com>
2026-03-26 23:05:23 -0300

    Update

---
+++ Services/Pdf/GeradorContratoPdf.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using Imob.Models;
+using System.Text;
+using PdfSharp;
+using PdfSharp.Drawing;
+using PdfSharp.Fonts;
+using PdfSharp.Pdf;
+using PdfSharp.Quality;
+using PdfSharp.Snippets.Font;
+
+namespace Imob.Services.Pdf
+{
+    internal class GeradorContratoPdf
+    {
+    }
+}
