9e8edee216d00b8cd690568afa84861bd4c17cdc
Guilherme-R-Santos <guilhermersantos99@outlook.com>
2026-06-18 21:34:44 -0300

    Update create pasta modelo contrato

---
+++ Models/Documentos/Contratos/ContratoLocacaoPdfModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Imob.Models.Documentos.Contratos
+{
+    public class ContratoLocacaoPdfModel
+    {
+        // TODO: Criar estrutura do modelo do contrato com os parágrafos para o contrato
+        // Após, modificar todo o processo de geração do contrato para usar esse modelo
+        // Separar por parágrafos, para facilitar a edição do contrato e a inserção de variáveis
+    }
+}
